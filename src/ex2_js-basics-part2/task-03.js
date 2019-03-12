3440010aa7aa7712dd5f0b387ad8ab7076a2a14b
Nikita Kazak <kitkazak@gmail.com>
2019-03-12 23:23:10 +0300

    fix countOddEvenZeroElements(arr)

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -2,14 +2,14 @@ var countOddEvenZeroElements = function(arr) {
     var result = [0, 0, 0];
 
     arr.forEach(element => {
-        if (element == null) {
+        if (element === null) {
         // null isn't considered as a zero element
             return;
-        } else if (element == 0) {
+        } else if (element === 0) {
             result[2]++;
-        } else if (element % 2 == 0) {
+        } else if (element % 2 === 0) {
             result[0]++;
-        } else if (element % 2 == 1) {
+        } else if (element % 2 === 1) {
             result[1]++;
         }        
     });
