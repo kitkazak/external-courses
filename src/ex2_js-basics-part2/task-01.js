4be969b84c89dadf3a5e4ef9d0fa1c88888a7617
Nikita Kazak <kitkazak@gmail.com>
2019-03-14 17:53:21 +0300

    isItNumberOrIsItString: add NaN type check

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,4 +1,6 @@
 var isItNumberOrIsItString = function(it) {
+    if (isNaN(it)) return undefined;
+
     if (typeof it === 'number' || typeof it === 'string') {
         return typeof it;    
     }
