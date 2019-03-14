552131a4cd5acbceb0fea5f371264fcd129e5898
Nikita Kazak <kitkazak@gmail.com>
2019-03-14 18:09:41 +0300

    isSimple: add check for numbers 0 and 1

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,4 +1,8 @@
 var isSimple = function(num) {
+    if (num === 0 || num === 1) {
+        return "Число " + num + " ни простое, ни составное";       
+    }
+
     if (num > 1000) return "Данные неверны";
 
     for (var i = 2; i < num; i++) {
