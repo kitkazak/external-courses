1c6c022146ebf1228b63b61ce0edd3216a772044
Nikita Kazak <kitkazak@gmail.com>
2019-03-19 18:18:34 +0300

    areElementsTheSame(arr): fix if-statement

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -2,8 +2,14 @@ var areElemsTheSame = function(arr) {
     var result;
     
     for (var i = 1; i < arr.length; i++) {
-        result = (arr[i] === arr[i - 1]) ? true : false;
-
+        // result = (arr[i] === arr[i - 1]) ? true : false;
+        
+        if (arr[i] === arr[i - 1]) {
+            result = true;
+        } else {
+            result = false;
+        }
+        
         if (result === false) break;
     }
 
