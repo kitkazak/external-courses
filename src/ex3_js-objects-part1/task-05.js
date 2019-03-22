f845534a785d7fa683f6a3bc1fe7e4f0f67fd2a2
Nikita Kazak <kitkazak@gmail.com>
2019-03-22 21:29:07 +0300

    define cloneObj(obj)

---
+++ src/ex3_js-objects-part1/task-05.js
@@ -0,0 +1,13 @@
+var cloneObj = function(obj) {
+    var clonedObj = {};
+
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            clonedObj[key] = obj[key];
+        }
+    }
+
+    return clonedObj;
+}
+
+module.exports = cloneObj;
