909a8f8421b3e72af06c10dd81a19f027ab18a27
Nikita Kazak <kitkazak@gmail.com>
2019-03-22 21:01:47 +0300

    define logKeysAndValues(obj)

---
+++ src/ex3_js-objects-part1/task-02.js
@@ -0,0 +1,10 @@
+var logKeysAndValues = function(obj) {
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(key + ': ' + obj[key]);
+        }
+    }
+
+}
+
+module.exports = logKeysAndValues;
