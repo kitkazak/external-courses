6c04c5b988909e31214dbecf5bfaf6f0f4fd3fe2
Nikita Kazak <kitkazak@gmail.com>
2019-04-08 18:14:13 +0300

    simplify checkProp(prop, obj)

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,14 +1,10 @@
 var checkProp = function(prop, obj) {
-    // linter doesn't allow to create new properties at object-argument 
-    var object = obj;  
+    var object = obj;
 
-    for (var key in object) {
-        if (object.hasOwnProperty(key) && key === prop) {
-            return object;
-        }
+    if (!object.hasOwnProperty(prop)) {
+        object[prop] = 'new';
     }
-    
-    object[prop] = 'new';
+
     return object;
 }
 
