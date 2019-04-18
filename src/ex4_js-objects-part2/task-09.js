70f9dfdffad5fec881a4ee044b9c7f6934ea07f6
Nikita Kazak <kitkazak@gmail.com>
2019-04-18 18:03:25 +0300

    remove extra spaces

--- src/ex4_js-objects-part2/task-09.js
+++ src/ex4_js-objects-part2/task-09.js
@@ -1,13 +1,11 @@
 var insertStrAfter = function(str, insertStr, wordIndex) {
     var words = str.split(" "), 
     wordsToInsert = insertStr.split(" ");
-
     for (var i = 0; i < wordsToInsert.length; i++) {
         words.splice(wordIndex + 1 + i, 0, wordsToInsert[i]);
     }
 
     return words.join(" ");
-
 }
 
-module.exports = insertStrAfter; 
+module.exports = insertStrAfter;
