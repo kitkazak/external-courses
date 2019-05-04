edaa7ea8ddb6a46babee2791b5f11e9b64fe3d14
Nikita Kazak <kitkazak@gmail.com>
2019-05-04 18:38:21 +0300

    define class AccountantCalculator

---
+++ src/ex15_js-oop/Calculator/AccountantCalculator/AccountantCalculator.js
@@ -0,0 +1,32 @@
+/**
+ * Бухгалтерский калькулятор
+ * Наследует от Calculator
+ */
+
+function AccountantCalculator() {
+    Calculator.call(this);
+}
+
+AccountantCalculator.prototype = Object.create(Calculator.prototype);
+AccountantCalculator.prototype.constuctor = AccountantCalculator;
+
+AccountantCalculator.prototype.round = function() {
+    this.__state = Math.round(this.__state);
+    return this;
+}
+
+AccountantCalculator.prototype.rubToUsd = function(rub) {
+    if (this.__isNumeric(rub)) {
+        console.log( `${parseFloat((rub / 60).toFixed(2))}$` );
+    } else {
+        console.log('Input value must be a number!');
+    }
+}
+
+AccountantCalculator.prototype.usdToRub = function(usd) {
+    if (this.__isNumeric(usd)) {
+        console.log( `${parseFloat((usd * 60).toFixed(2))}₽`);
+    } else {
+        console.log('Input value must be a number!');
+    }
+}
