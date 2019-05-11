416365a9647fd3c032409141074690c4ee8a56aa
Nikita Kazak <kitkazak@gmail.com>
2019-05-11 22:59:59 +0300

    redo addNewNoteToHistory

--- src/library/scripts/ratingChange.js
+++ src/library/scripts/ratingChange.js
@@ -1,32 +1,24 @@
 window.addEventListener('ratingChange', addNewNoteToHistory);
 
 function addNewNoteToHistory(e) {
-    // new note's components
     var historySection = document.querySelector('.sidebar__history ul'),
-    noteContainer = document.createElement('li'),
-    noteIcon = document.createElement('i'),
-    noteTextContainer = document.createElement('div'),
-    noteParagraph = document.createElement('p'),
-    noteAuthor = document.createElement('b'),
-    noteTitle = document.createElement('b'),
-    noteTime = document.createElement('span');
+        newNote = document.createElement('li');
 
-    noteIcon.className = 'far fa-clock';
-    noteTextContainer.className = 'sidebar__history-text';
-    noteAuthor.className = 'sidebar__history-author';
-    noteTitle.className = 'sidebar__history-title';
-    noteTime.className = 'sidebar__history-time';
-    
-    noteParagraph.innerHTML = 
-        `You changed rating of 
-        <b class="sidebar__history-title">${e.detail.changedBookName}</b> by 
-        <b class="sidebar__history-author">${e.detail.changedBookAuthor}</b>`;
-    noteTime.innerHTML = 'Just Now!';
+    newNote.innerHTML = 
+    `
+    <i class="far fa-clock"></i>
+    <div class="sidebar__history-text">
+        <p>
+            You changed rating of
+            <b class="sidebar__history-title">${e.detail.changedBookName}</b>
+            by
+            <b class="sidebar__history-author">${e.detail.changedBookAuthor}</b>
+        </p>
+        <span class="sidebar__history-time">
+            Just Now!
+        </span>
+    </div>
+    `;
 
-    noteTextContainer.prepend(noteTime);
-    noteTextContainer.prepend(noteParagraph);
-    noteContainer.prepend(noteTextContainer);
-    noteContainer.prepend(noteIcon);
-
-    historySection.prepend(noteContainer);
+    historySection.prepend(newNote);
 }
