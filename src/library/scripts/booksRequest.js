fe18c178dddcccbc52dabd986e1217a92063ee65
Nikita Kazak <kitkazak@gmail.com>
2019-05-17 23:13:44 +0300

    add filters

--- src/library/scripts/booksRequest.js
+++ src/library/scripts/booksRequest.js
@@ -1,3 +1,15 @@
+var loadedBooksArr = [];
+
+function JSONToBooks(JSON) {
+    loadedBooksArr = JSON;
+
+    var booksSection = document.querySelector('.books__container');
+    JSON.forEach(function(obj) {
+        var book = new Book(obj);
+        booksSection.append(book.getDOMElement())
+    })
+}
+
 const booksRequest = () => {
     const url = 'https://rsu-library-api.herokuapp.com/books';
 
@@ -5,14 +17,6 @@ const booksRequest = () => {
     .then(res => res.json())
     .then(JSONToBooks)
     .catch(console.log)
-
-    function JSONToBooks(JSON) {
-        var booksSection = document.querySelector('.books__container');
-        JSON.forEach(function(obj) {
-            var book = new Book(obj);
-            booksSection.append(book.getDOMElement())
-        })
-    }
 }
 
 window.addEventListener('load', booksRequest);
---
+++ src/library/scripts/filterBooks.js
@@ -0,0 +1,35 @@
+function filterBooks(filterName) {
+    var filteredBooksArr = loadedBooksArr.filter(book => {
+        switch (filterName) {
+            case 'popular': 
+                if (book.rating === 5) return true;
+                break;
+            case 'free': 
+                if (book.cost === 0) return true;
+                break;
+            default:
+                return true;
+        }
+
+        return false;
+    });
+
+    if (filterName === 'recent') {
+        console.log('recent');
+        filteredBooksArr.sort((a, b) => {
+            return a.createdAt > b.createdAt;
+        });
+    }
+
+    renderBooks(filteredBooksArr);
+}
+
+function renderBooks(booksArr) {
+    var booksContainer = document.querySelector('.books__container');
+    booksContainer.innerHTML = '';
+
+    for (let i = 0; i < booksArr.length; i++) {
+        var book = new Book(booksArr[i]);
+        booksContainer.appendChild(book.getDOMElement());
+    }
+}
